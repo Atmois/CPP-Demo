d5ee2db0699a9f449f0b613fd77af305d36b87c3
Atmois <130537361+Atmois@users.noreply.github.com>
2023-11-01 17:55:02 +0100

    improve demo funct

--- 26. Functions/main.cpp
+++ 26. Functions/main.cpp
@@ -9,20 +9,20 @@ Functions can share names but must have differnt paramters.
 Function prefix before name should be named by the datatype processed within the function.
 */
 
-int exampleFunction(int num1, int num2) { 
-    int ans = num1 + num2;
+double add(double num1, double num2) { 
+    double ans = num1 + num2;
     return ans;
 }
 
 int main()
 {
-    int num1;
-    int num2;
+    double num1;
+    double num2;
 
     std::cin >> num1;
     std::cin >> num2;
 
-    int ans = exampleFunction(num1, num2); // Ensure you set a function = to a var if taking a return
+    double ans = add(num1, num2); // Ensure you set a function = to a var if taking a return
     std::cout << ans;
 
     return 0;
