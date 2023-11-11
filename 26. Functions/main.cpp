75b6b89649d83602bf226fcfd749668e068a9658
Atmois <atmois@outlook.com>
2023-11-11 15:59:50 +0000

    passing

--- 26. Functions/main.cpp
+++ 26. Functions/main.cpp
@@ -4,7 +4,7 @@
 Any extra functions need to be above the main function.
 To pass arguments put var type before var name in function().
 Functions can share names but must have differnt paramters.
-Function prefix before name should be named by the datatype processed within the function.
+Function prefix before name should be named by the datatype processed within the function or 'void'..
 */
 
 double add(double num1, double num2)
