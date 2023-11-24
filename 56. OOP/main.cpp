e8dfc77bf6e3b4abf6f6995e383d6843ffe8a805
Atmois <atmois@outlook.com>
2023-11-24 09:00:23 +0000

    add inheritance

--- 56. OOP/main.cpp
+++ 56. OOP/main.cpp
@@ -19,6 +19,13 @@ public:
     }
 };
 
+/*
+Inheritance
+*/
+class Tablet : public Phone
+{ // Exists to allow this class to use everything the parent class can do as well as extra functionality
+};
+
 int main() // Collection of attributes (Characteristics) & methods (What can it do) - Created from a class
 {
     Phone phone1; // Declares variable as object
@@ -32,6 +39,15 @@ int main() // Collection of attributes (Characteristics) & methods (What can it 
     std::cout << "\n";
 
     std::cout << phone1.brand << " " << phone1.series << " " << phone1.model;
+    std::cout << "\n";
+
+    /*
+    Inheritance
+    */
+    Tablet tablet1;
+    tablet1.series = 9; 
+    tablet1.model = "Air";
+    std::cout << tablet1.brand << " " << tablet1.model << " " << tablet1.series;
 
     return 0;
 }
